f28f4186346d82324ae00fce4b6b429a44c6c627
Ruben <nereperea@gmail.com>
2016-11-23 20:13:55 -0600

    Complete la seccion heroe grande y introduccion  a contenido bloque

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'), 
+     postcss = require('gulp-postcss'),
+     autoprefixer = require('autoprefixer'),
+     cssvars = require('postcss-simple-vars'),
+     cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins'),
+     nested = require('postcss-nested');
+
+gulp.task('styles', function(){
+   return gulp.src('./app/assets/styles/style.css').
+   pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+   .on('error', function(errorInfo){
+       console.log(errorInfo.toString());
+       this.emit('end');
+   })
+    .pipe(gulp.dest('./app/temp/style'));
+});
